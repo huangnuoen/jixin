8412208cb71a9aae5afd781209f70c510bf3d201
hne <huangnuoen@gmail.com>
2017-04-28 14:28:26 +0800

    cn

---
+++ Contacts/contacts.js
@@ -0,0 +1,29 @@
+//获得id
+function checkID (id) {
+	var $div = $('.contacts .col-md-9');
+	console.log($div);
+	for (var i = 0; i < $div.length; i++) {
+		if($div[i].getAttribute('id') != id) {
+			$div[i].style.display = 'none';
+		} else {
+			$div[i].style.display = 'block';
+		}
+	};
+}
+//获得href
+window.onload = function() {
+	var $links = $('.contacts .col-md-3 ul li a');
+	for (var i = 0; i < $links.length; i++) {
+		var href = $links[i].getAttribute('href');
+		var id = href.split('#')[1];
+		document.getElementById(id).style.display = 'none';
+		$links[i].destination = id;
+		$links[i].onclick = function() {
+			checkID(this.destination);
+			console.log($(this).parent().className == 'selected');
+			//return false;
+		}
+	};
+}
+
+
