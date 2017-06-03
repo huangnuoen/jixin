e169a4f0325874c601f4c7a603b28164b33dd1ec
hne <huangnuoen@gmail.com>
2017-06-03 11:35:33 +0800

    表单验证-focus && blur

--- private/ife29/form.js
+++ private/ife29/form.js
@@ -6,7 +6,9 @@ function $all(a) {
 }
 var btn = $('button'),
 	input = $all('input');
-	btn.onclick = submit;
+//将nodelist转化为array
+var arrInput = [].slice.call(input);
+var wrongTip = ['名称长度必须在4-16字符之间', '请输入6到16位字符且只能为数字和字母', '两次密码必须一致', '电子邮箱不合法', '手机号码格式错误'];
 
 function check() {
 	document.getElementsByTagName('input')[0].className = '';		
@@ -45,10 +47,23 @@ function sumbit() {
 		alert('填写数据不合格');
 	}
 }
-function init(){
-	var input = $all('input');
-	for(var i=0; i< input.length; i++) {
-		input[i].onfocus = alert('2');
-	}
+//为每个input添加事件
+for(var i = 0 ; i < arrInput.length; i++) {
+	console.log(arrInput[i]);
+	arrInput[i].addEventListener('focus', function(e){
+		var index = arrInput.indexOf(e.target);
+		var span = this.parentNode.querySelector('span');
+		span.className = "visible";
+	});
+	arrInput[i].addEventListener('blur', function(e){
+		var index = arrInput.indexOf(e.target);
+		var span = this.parentNode.querySelector('span');
+		span.innerHTML = '不能为空';
+		span.className = 'visible ' + 'wrong';
+		this.style.borderColor = 'red';
+	});
 }
-init();
+
+// input[0].addEventListener('focus', function(e) {
+// 	sumbit();
+// })
--- private/ife29/readme.txt
+++ private/ife29/readme.txt
@@ -1,1 +1,5 @@
 https://huangnuoen.github.io/jixin/private/ife29/form.html
+
+为每个input绑定focus 、blur事件，
+focus:出现提示，
+blur:
