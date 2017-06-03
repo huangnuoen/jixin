6a26ce4994cd6f110f19464ca19e629bb1da4ec9
hne <huangnuoen@gmail.com>
2017-06-03 17:38:23 +0800

    2

---
+++ private/ife3-3/form.js
@@ -0,0 +1,9 @@
+var area = {
+	BJ: ['清华大学', '人民大学', '科技大学', '北京大学'],
+	GD: ['中山大学', '华南理工', '广州大学', '深圳大学'],
+	SH: [ '交通大学', '复旦大学', ]
+}
+function getInput () {
+	// body...
+}
+var input = document.querySelector('input')
