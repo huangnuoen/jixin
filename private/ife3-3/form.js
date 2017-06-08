ca4c7394bbeda1d4f88428ff5e423e07c6534d07
hne <huangnuoen@gmail.com>
2017-06-08 15:41:28 +0800

    表单联动

--- private/ife3-3/form.js
+++ private/ife3-3/form.js
@@ -4,6 +4,44 @@ var area = {
 	SH: [ '交通大学', '复旦大学', ]
 }
 function getInput () {
-	// body...
+	for (var i = 0; i < radio.length; i++) {
+		if(radio[i].checked) {
+			console.log(radio[i].value);
+			var school = document.querySelector('#area');	
+			var work = document.querySelector('#work');
+			if(radio[i].value == 'worker') {
+				work.style.display = 'block';
+				school.style.display = 'none';
+			} else {
+				var school = document.querySelector('#area');
+				school.style.display = 'block';
+				work.style.display = 'none';
+			}
+		}
+	};
 }
-var input = document.querySelector('input')
+var radio = document.getElementsByName('job');
+for (var i = 0; i < radio.length; i++) {
+	radio[i].onclick = getInput;
+};
+function showSchool() {
+	var select = document.querySelector('#school');
+	console.log(select.value);
+	var index = select.selectedIndex;
+	var newOption = '';
+	for(var k in area) {
+		if(select.value == k) {
+			//生成相应option
+			for (var j = 0; j < area[k].length; j++) {
+				newOption += '<option>' + area[k][j] + '</option>';
+			};
+		}
+	}
+	var shoolSelect = document.getElementById('new');
+	shoolSelect.innerHTML = newOption;
+}
+var school = document.querySelector('#school');
+//select状态改变时，执行showSchool函数
+school.onchange = showSchool;
+//文档开始加载时先执行一次showSchool
+showSchool();
---
+++ private/ife3-3/readme.txt
@@ -0,0 +1,7 @@
+﻿https://huangnuoen.github.io/jixin/private/ife3-3/form.html
+
+点击input获取value,判断哪个div显示
+选择select,根据点击的option,生成相应的select和option:
+1、获得当前selected值
+2、与对象area进行判断
+3、匹配后显示对应option
